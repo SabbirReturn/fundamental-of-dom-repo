09cf8ebafd3216cbab80f9056dd766d5a415c096
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-01-05 03:36:12 +0600

    summary

---
+++ practice1/js/index.js
@@ -0,0 +1,16 @@
+// let li = document.getElementsByTagName('li')
+// console.log(li);
+// console.log(88);
+
+let change = document.getElementById('section-1')
+change.classList.add('change')
+
+
+
+let liCollection = document.getElementById('sectionli');
+
+let li = document.createElement('li');
+li.innerText = 'Dynamic';
+
+liCollection.appendChild(li);
+
