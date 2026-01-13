1a006e22249fb0f8c8ac091c57525a6c22622dec
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-01-14 02:58:02 +0600

    learn funtion when click option is open

---
+++ dom-events/js/event.js
@@ -0,0 +1,42 @@
+function makeBlue(){
+    document.body.style.backgroundColor = 'Blue'
+}
+
+let yellow = document.getElementById('makeYellow');
+yellow.onclick = makeYellow;
+
+function makeYellow (){
+    document.body.style.backgroundColor = 'yellow'
+}
+
+// let purple = document.getElementById('makePurple');
+// purple.onclick = function (){
+//     document.body.style.backgroundColor= 'purple'
+// }
+// let purple = document.getElementById('makePurple');
+// purple.onclick = function (){
+//     document.body.style.backgroundColor= 'purple'
+// }
+let purple = document.getElementById('makePurple');
+purple.onclick = function (){
+    document.body.style.backgroundColor= 'purple'
+}
+
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+let white = document.getElementById('makeWhite');
+white.onclick = function(){
+    document.body.style.backgroundColor = 'White';
+}
+
+let greenBtn = document.getElementById('makeGreen');
+greenBtn.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'green';
+})
+
+document.getElementById('makeGoldenRod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod'
+})
