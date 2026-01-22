01b421a9c4b2232b76f6ee293e219d73d1c33f4e
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-01-23 04:20:42 +0600

    practice

---
+++ dom-events/js/github.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById('inputField').addEventListener('keyup', function(event){
+    let defaultValue = event.target.value;
+    // console.log(defaultValue)
+    let deleteBtn = document.getElementById('deleteBtn')
+    if(defaultValue === 'delete'){
+        deleteBtn.removeAttribute('disabled');
+    }
+    else{
+        deleteBtn.setAttribute('disabled',true);
+    }
+})
+
+
+
+document.getElementById('deleteBtn').addEventListener('click',function(){
+    let defaultText = document.getElementById('text');
+    defaultText.style.display = 'none';
+})
