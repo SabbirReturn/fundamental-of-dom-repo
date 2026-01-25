afa8fb3cf9ba4ccc475ee4612329041271cdbade
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-01-25 22:22:03 +0600

    again

--- dom-events/js/event_bubble.js
+++ dom-events/js/event_bubble.js
@@ -2,6 +2,10 @@ document.getElementById('item-2').addEventListener('click', function(event){
     console.log('Item-2 click');
     event.stopPropagation();
 })
+// document.getElementById('list_container').addEventListener('click', function(){
+//     console.log('Li clicked')
+// })
+
 document.getElementById('list_container').addEventListener('click', function(){
     console.log('Li clicked')
 })
