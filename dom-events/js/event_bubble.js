768573ea3a4d4e255208758b43fd1c099445d00e
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-02-01 03:29:17 +0600

    again

--- dom-events/js/event_bubble.js
+++ dom-events/js/event_bubble.js
@@ -2,28 +2,6 @@ document.getElementById('item-2').addEventListener('click', function(event){
     console.log('Item-2 click');
     event.stopPropagation();
 })
-// document.getElementById('list_container').addEventListener('click', function(){
-//     console.log('Li clicked')
-// })
-
-// document.getElementById('list_container').addEventListener('click', function(){
-//     console.log('Li clicked')
-// })
-// document.getElementById('list_container').addEventListener('click', function(){
-//     console.log('Li clicked')
-// })
-// document.getElementById('list_container').addEventListener('click', function(){
-//     console.log('Li clicked')
-// })
-// document.getElementById('list_container').addEventListener('click', function(){
-//     console.log('Li clicked')
-// })
-// document.getElementById('list_container').addEventListener('click', function(){
-//     console.log('Li clicked')
-// })
-// document.getElementById('list_container').addEventListener('click', function(){
-//     console.log('Li clicked')
-// })
 document.getElementById('list_container').addEventListener('click', function(){
     console.log('Li clicked')
 })
--- dom-events/js/github.js
+++ dom-events/js/github.js
@@ -1,20 +1,35 @@
 
 
-document.getElementById('inputField').addEventListener('keyup', function(event){
-    let defaultValue = event.target.value;
-    // console.log(defaultValue)
+// document.getElementById('inputField').addEventListener('keyup', function(event){
+//     let defaultValue = event.target.value;
+//     // console.log(defaultValue)
+//     let deleteBtn = document.getElementById('deleteBtn')
+//     if(defaultValue === 'delete'){
+//         deleteBtn.removeAttribute('disabled');
+//     }
+//     else{
+//         deleteBtn.setAttribute('disabled',true);
+//     }
+// })
+
+// document.getElementById('deleteBtn').addEventListener('click',function(){
+//     let defaultText = document.getElementById('text');
+//     defaultText.style.display = 'none';
+// })
+
+document.getElementById('textInput').addEventListener('keyup', function(event){
+    let textInput = event.target.value;
+    // console.log(textInput);
     let deleteBtn = document.getElementById('deleteBtn')
-    if(defaultValue === 'delete'){
-        deleteBtn.removeAttribute('disabled');
+    if(textInput == 'delete'){
+        deleteBtn.removeAttribute('disabled')
     }
     else{
-        deleteBtn.setAttribute('disabled',true);
+        deleteBtn.setAttribute('disabled', true)
     }
 })
 
-
-
-document.getElementById('deleteBtn').addEventListener('click',function(){
-    let defaultText = document.getElementById('text');
-    defaultText.style.display = 'none';
+document.getElementById('deleteBtn').addEventListener('click', function(){
+    let text = document.getElementById('defaultText');
+    text.style.display= 'none';
 })
